47f310f3587685fe233a96d191e0648ae828f3d7
VamshiBakaram <vamsi.bakaram@ahex.co.in>
2026-04-22 15:39:38 +0530

    Initial Commit

---
+++ CICD_Pipelines/CICD_PipelinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_PipelinesApp.swift
+//  CICD_Pipelines
+//
+//  Created by Ahex on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CICD_PipelinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
